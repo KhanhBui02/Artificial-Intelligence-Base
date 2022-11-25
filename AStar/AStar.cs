a2be192881be1476027aa9a1d1eb8663e37b42cf
KhanhBui02 <buidoanbuukhanh0123456@gmail.com>
2022-11-25 09:18:25 +0700

    Done Astar

--- AStar/AStar.cs
+++ AStar/AStar.cs
@@ -3,7 +3,7 @@ using System.Xml.Linq;
 
 class AStar
 {
-    private class NodeAstar
+    public class NodeAstar
     {
         public List<Link> links;
         public NodeAstar parentNode;
@@ -22,11 +22,19 @@ class AStar
             this.f = f;
         }
 
+        public NodeAstar(List<Link> afterNodes, PosInArray posInArray)
+        {
+            this.links = afterNodes;
+            this.posInArray = posInArray;
+            estimateCost = 0;
+            d = 0;
+            f = 0;
+        }
+
         public NodeAstar(List<Link> afterNodes)
         {
             this.links = afterNodes;
             posInArray = new PosInArray();
-            parentNode = new NodeAstar();
             estimateCost = 0;
             d = 0;
             f = 0;
@@ -35,7 +43,6 @@ class AStar
         public NodeAstar()
         {
             this.links = new List<Link>();
-            parentNode = new NodeAstar();
             posInArray = new PosInArray(-1, -1);
             estimateCost = 0;
             d = 0;
@@ -43,7 +50,7 @@ class AStar
         }
     }
 
-    private class PosInArray
+    public class PosInArray
     {
         public int column;
         public int row;
@@ -66,7 +73,7 @@ class AStar
         }
     }
 
-    private class Link
+    public class Link
     {
         public NodeAstar nodeAstar;
         public int weight;
@@ -86,14 +93,15 @@ class AStar
     private List<NodeAstar> _closedList = new List<NodeAstar>();
     private LinkedList<NodeAstar> rightWay = new LinkedList<NodeAstar>();
 
-    private AStar(NodeAstar[,] matrix, PosInArray startPos, PosInArray endPos)
+    public AStar(NodeAstar[,] matrix, PosInArray startPos, PosInArray endPos)
     {
-        _startNodeAstar = matrix[startPos.column, startPos.row];
-        _endNodeAstar = matrix[endPos.column, endPos.row];
-        _matrix = matrix;
+        _matrix = ConvertRow_ColToCol_Row(matrix);
+        //SetupPosInArray(_matrix);
+
+        _startNodeAstar = matrix[startPos.row, startPos.column];
+        _endNodeAstar = matrix[endPos.row, endPos.column];
 
         CalculateEstimateCost(_matrix);
-        SetupPosInArray(_matrix);
     }
 
     private void CalculateEstimateCost(NodeAstar[,] matrix)
@@ -103,6 +111,7 @@ class AStar
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 matrix[i,j].estimateCost = _endNodeAstar.posInArray.TotalBySubWithAbs(matrix[i, j].posInArray);
+                //matrix[j, i].estimateCost = _endNodeAstar.posInArray.TotalBySubWithAbs(matrix[j, i].posInArray);
             }
         }
     }
@@ -116,6 +125,21 @@ class AStar
                 matrix[i, j].posInArray = new PosInArray(i, j);
             }
         }
+    }
+
+    private NodeAstar[,] ConvertRow_ColToCol_Row(NodeAstar[,] matrix)
+    {
+        NodeAstar[,] newMatrix = new NodeAstar[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < newMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < newMatrix.GetLength(1); j++)
+            {
+                newMatrix[i, j] = matrix[j, i];
+            }
+        }
+
+        return newMatrix;
     }
 
     private void Travelled(NodeAstar currentNode)
@@ -188,6 +212,8 @@ class AStar
 
     private void PrintAction()
     {
+        _closedList.Add(_startNodeAstar);
+
         Travelled(_startNodeAstar);
 
         GetRightWay(_closedList.Last());
@@ -206,6 +232,11 @@ class AStar
             rightWay.AddFirst(nodeAstar);
         }
 
+        if (nodeAstar.posInArray.Equals(_startNodeAstar.posInArray))
+        {
+            return;
+        }
+
         if (nodeAstar.parentNode == null) return;
 
         rightWay.AddFirst(nodeAstar.parentNode);
@@ -216,16 +247,66 @@ class AStar
     private static NodeAstar[,] SetupMatrixData()
     {
         List<Link> links = new List<Link>();
+        PosInArray pos;
 
-        NodeAstar node0_0 = new NodeAstar(links);
+        /*links = new List<Link>();
         NodeAstar node0_1 = new NodeAstar(links);
+
+        links = new List<Link>();
         NodeAstar node0_2 = new NodeAstar(links);
+
+        links = new List<Link>();
         NodeAstar node1_0 = new NodeAstar(links);
+
+        links = new List<Link>();
         NodeAstar node1_1 = new NodeAstar(links);
+
+        links = new List<Link>();
         NodeAstar node1_2 = new NodeAstar(links);
+
+        links = new List<Link>();
         NodeAstar node2_0 = new NodeAstar(links);
+
+        links = new List<Link>();
         NodeAstar node2_1 = new NodeAstar(links);
-        NodeAstar node2_2 = new NodeAstar(links);
+
+        links = new List<Link>();
+        NodeAstar node2_2 = new NodeAstar(links);*/
+
+        pos = new PosInArray(0, 0);
+        NodeAstar node0_0 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(0, 1);
+        NodeAstar node0_1 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(0, 2);
+        NodeAstar node0_2 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(1, 0);
+        NodeAstar node1_0 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(1, 1);
+        NodeAstar node1_1 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(1, 2);
+        NodeAstar node1_2 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(2, 0);
+        NodeAstar node2_0 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(2, 1);
+        NodeAstar node2_1 = new NodeAstar(links, pos);
+
+        links = new List<Link>();
+        pos = new PosInArray(2, 2);
+        NodeAstar node2_2 = new NodeAstar(links, pos);
 
         node0_0.links.Add(new Link(node0_1, 2));
         node0_0.links.Add(new Link(node1_0, 5));
@@ -259,6 +340,9 @@ class AStar
             {node0_0, node1_0, node2_0 },
             {node0_1, node1_1, node2_1 },
             {node0_2, node1_2, node2_2 },
+            /*{node0_0, node0_1, node0_2},
+            {node1_0, node1_1, node1_2},
+            {node2_0, node2_1, node2_2 },*/
         };
 
         return matrix;
